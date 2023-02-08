256d2674ec8de49ca84adca44df1cd85d13530f6
hotaroo-dev <kay.sothearo2019@rupp.edu.kh>
2023-02-08 16:13:55 +0700

    feat(image): use gatsby image
    Use the GatsbyImage component if the image source changes for different instances of your component
    (like if it gets passed in as a prop)

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,14 +1,18 @@
 import React, { ReactNode } from 'react'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 import type { HeadFC } from 'gatsby'
+import type { IGatsbyImageData } from 'gatsby-plugin-image'
 
 interface IData {
   mdx: {
     frontmatter: {
       date: string
       title: string
+      hero_image_alt: string
+      hero_image: IGatsbyImageData
     }
   }
 }
@@ -19,9 +23,12 @@ interface Props {
 }
 
 const BlogPost: React.FC<Props> = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image) as IGatsbyImageData
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
       {children}
     </Layout>
   )
@@ -32,7 +39,13 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
